334bb88add7501cb919113fa4a2a82fcab7971b8
durga prasad mudhuru <mdprasadeng@gmail.com>
2017-11-13 12:16:44 +0530

    basic layout

--- content/decimal-system/Game.js
+++ content/decimal-system/Game.js
@@ -1,15 +1,29 @@
-/// <reference path="../../typescript/phaser.d.ts" />
+/// <reference path="../../typescript/phaser.comments.d.ts" />
+var DonutLayout = /** @class */ (function () {
+    function DonutLayout() {
+    }
+    DonutLayout.width = 900;
+    DonutLayout.height = 1500;
+    DonutLayout.hud = 100;
+    DonutLayout.donut = 66;
+    DonutLayout.block = DonutLayout.donut + 10;
+    DonutLayout.donuts_hands = DonutLayout.block * 5;
+    DonutLayout.number_garbage = DonutLayout.block * 3;
+    DonutLayout.donut_cases_padding = 50;
+    return DonutLayout;
+}());
+var State;
+(function (State) {
+    State[State["WAITING_FOR_CASE"] = 0] = "WAITING_FOR_CASE";
+    State[State["CASE_SELECTED"] = 1] = "CASE_SELECTED";
+})(State || (State = {}));
 var Game = /** @class */ (function () {
-    function Game() {
+    function Game(width, height) {
         var _this = this;
         this.donuts = [];
-        this.donut_holders = [];
-        this.aspect_ratio = { width: 900, height: 1600 };
-        this.donuts_center = { x: 450, y: 180 };
-        this.donuts_radius = 130;
-        this.donut_size = 62;
-        this.donut_block = this.donut_size + 8;
-        this.game = new Phaser.Game(this.aspect_ratio.width, this.aspect_ratio.height, Phaser.AUTO, "content", {
+        this.cases = [];
+        this.selection_made = false;
+        this.game = new Phaser.Game(width, height, Phaser.AUTO, "content", {
             preload: function () { return _this.preload(); },
             create: function () { return _this.create(); },
             update: function () { return _this.update(); }
@@ -20,64 +34,125 @@ var Game = /** @class */ (function () {
         this.game.stage.backgroundColor = 0x3f7cb6;
     };
     Game.prototype.create = function () {
+        this.centerRootGroup();
+        this.initHud();
+        this.initDonutHands();
+        this.initNumber();
+        this.initDonutCases();
+        this.initGarbage();
+    };
+    Game.prototype.initNumber = function () {
+        this.number = this.game.add.group(this.root, "number_garbage");
+        this.number.y = this.donuts_hands.y + DonutLayout.donuts_hands;
+    };
+    Game.prototype.initDonutCases = function () {
         var _this = this;
-        var holder_group = this.game.add.group();
-        holder_group.x = 100;
-        holder_group.y = 700;
-        var style = {
-            font: "32px Arial",
-            fill: "black",
-            wordWrap: true,
-            wordWrapWidth: this.donut_block,
-            align: "center"
-        };
-        var donut_scale = this.donut_size / this.game.cache.getFrameByName("donuts", "donut_1").width;
-        var rect = new Phaser.Rectangle(holder_group.x, this.donut_block, this.donut_block * 9, 2 * this.donuts_radius);
-        var circle = new Phaser.Circle(this.donuts_center.x, this.donuts_center.y, 2 * this.donuts_radius);
-        var rnd_point = new Phaser.Point();
+        var donut_scale = DonutLayout.donut / this.game.cache.getFrameByName("donuts", "donut_1").width;
+        this.donuts_cases = this.game.add.group(this.root, "donuts_cases");
+        this.donuts_cases.y = this.number.y + DonutLayout.number_garbage;
+        this.donuts_cases.x = DonutLayout.donut_cases_padding;
         var _loop_1 = function (index) {
-            var x = holder_group.x + (index % 10) * this_1.donut_block + this_1.game.rnd.realInRange(-this_1.donut_block / 2, this_1.donut_block / 2);
-            var y = this_1.donut_block + this_1.game.rnd.realInRange(0, 2 * this_1.donuts_radius);
-            rect.random(rnd_point);
-            this_1.donuts[index] = this_1.game.add.spriteBatch(null, "donut_" + (index + 1));
-            this_1.rnd_donut().forEach(function (name, layer_index) {
-                var layer = _this.donuts[index].create(x + _this.donut_block / 2, y + _this.donut_block / 2, "donuts", name);
-                layer.anchor.setTo(0.5, 0.5);
-                layer.scale.setTo(donut_scale, donut_scale);
-            });
+            var x = (index % 10) * DonutLayout.block;
+            var y = Math.floor(index / 10) * DonutLayout.block;
+            this_1.cases[index] = this_1.game.add.group(this_1.donuts_cases);
+            this_1.cases[index].x = x;
+            this_1.cases[index].y = y;
+            var graphics = this_1.game.add.graphics(0, 0, this_1.cases[index]);
+            graphics.lineStyle(3, 0x404040, 1);
+            graphics.drawRect(0, 0, DonutLayout.block, DonutLayout.block);
             //let text = this.game.add.text(x + this.donut_block / 2, y + this.donut_block / 2, String(index + 1), style, this.donut_holders[index])
             //text.anchor.setTo(0.5, 0.5)
-        };
-        var this_1 = this;
-        for (var index = 0; index < 15; index++) {
-            _loop_1(index);
-        }
-        for (var index = 0; index < 100; index++) {
-            var x = (index % 10) * this.donut_block;
-            var y = Math.floor(index / 10) * this.donut_block;
-            this.donut_holders[index] = this.game.add.group(holder_group);
-            // let graphics = this.game.add.graphics(0, 0, holder_group)
-            // graphics.lineStyle(1, 0x585858, 0.6);
-            // graphics.drawRect(x, y, this.donut_block, this.donut_block)
-            //let text = this.game.add.text(x + this.donut_block / 2, y + this.donut_block / 2, String(index + 1), style, this.donut_holders[index])
-            //text.anchor.setTo(0.5, 0.5)
-            var donut_holder = this.donut_holders[index].create(x + this.donut_block / 2, y + this.donut_block / 2, "donuts", "donut_black");
+            var donut_holder = this_1.cases[index].create(DonutLayout.block / 2, DonutLayout.block / 2, "donuts", "donut_black");
             donut_holder.anchor.setTo(0.5, 0.5);
             donut_holder.scale.setTo(donut_scale, donut_scale);
             donut_holder.alpha = 1;
+            donut_holder.data.index = index;
+            donut_holder.inputEnabled = true;
+            donut_holder.events.onInputOver.add(function () {
+                if (_this.selection_made == false) {
+                    _this.case_selected = donut_holder.data.index;
+                    _this.previewCaseSelected(0.3);
+                    console.log("Over ", donut_holder.data.index);
+                }
+            });
+            donut_holder.events.onInputUp.add(function () {
+                _this.case_selected = donut_holder.data.index;
+                _this.selection_made = true;
+                console.log("Up on ", donut_holder.data.index);
+                _this.previewCaseSelected(0);
+                _this.caseDonuts();
+            });
+        };
+        var this_1 = this;
+        for (var index = 0; index < 100; index++) {
+            _loop_1(index);
         }
-        var hand_frame = this.game.cache.getFrameByName("donuts", "right_hand");
-        var hand_scale = this.donut_block * 4 / hand_frame.width;
-        this.hands = this.game.add.group();
+    };
+    Game.prototype.initGarbage = function () {
+        var garbage_scale = DonutLayout.number_garbage / (this.game.cache.getFrameByName("donuts", "recycle_bin").height + 10);
+        this.garbage = this.game.add.sprite(DonutLayout.block * 10, 0, "donuts", "recycle_bin", this.donuts_cases);
+        this.garbage.anchor.setTo(0.5, 0);
+        this.garbage.scale.setTo(garbage_scale, garbage_scale);
+        this.garbage.alpha = 0;
+    };
+    Game.prototype.initDonutHands = function () {
+        var _this = this;
+        var donut_scale = DonutLayout.donut / this.game.cache.getFrameByName("donuts", "donut_1").width;
+        this.donuts_hands = this.game.add.group(this.root, "donuts_hands");
+        this.donuts_hands.y = DonutLayout.hud;
+        this.donuts_hands.x = DonutLayout.donut_cases_padding;
+        var _loop_2 = function (index) {
+            var x = (index % 10 + this_2.game.rnd.realInRange(-0.5, 0.5)) * DonutLayout.block;
+            var y = this_2.game.rnd.realInRange(0, DonutLayout.donuts_hands - DonutLayout.block);
+            this_2.donuts[index] = this_2.game.add.spriteBatch(this_2.donuts_hands, "donut_" + (index + 1));
+            this_2.donuts[index].position.x = x + DonutLayout.block / 2;
+            this_2.donuts[index].position.y = y + DonutLayout.block / 2;
+            this_2.rnd_donut().forEach(function (name, layer_index) {
+                var layer = _this.donuts[index].create(0, 0, "donuts", name);
+                layer.anchor.setTo(0.5, 0.5);
+                layer.scale.setTo(donut_scale, donut_scale);
+            });
+        };
+        var this_2 = this;
+        for (var index = 0; index < 10; index++) {
+            _loop_2(index);
+        }
+        this.initHands();
+    };
+    Game.prototype.initHands = function () {
+        this.hands = this.game.add.group(this.donuts_hands, "hands");
         this.hands.alpha = 0.8;
-        this.right_hand = this.game.add.sprite(this.donut_block * 5, 0, "donuts", "right_hand", this.hands);
-        this.right_hand.anchor.setTo(0.5, 0);
+        var hand_scale = DonutLayout.donuts_hands / this.game.cache.getFrameByName("donuts", "right_hand").width;
+        this.right_hand = this.game.add.sprite(DonutLayout.donuts_hands, 0, "donuts", "right_hand", this.hands);
         this.right_hand.scale.setTo(hand_scale, hand_scale);
-        this.left_hand = this.game.add.sprite(0, 0, "donuts", "right_hand", this.hands);
+        this.left_hand = this.game.add.sprite(DonutLayout.donuts_hands / 2, 0, "donuts", "right_hand", this.hands);
         this.left_hand.anchor.setTo(0.5, 0);
         this.left_hand.scale.setTo(-hand_scale, hand_scale);
-        this.hands.x = holder_group.x + this.donut_block * 2.5;
-        this.hands.y = holder_group.y - this.right_hand.height;
+    };
+    Game.prototype.initHud = function () {
+        this.hud = this.game.add.group(this.root, "hud");
+        this.hud.x = DonutLayout.donut_cases_padding;
+        this.timer = this.game.add.text(0, 0, "1:30", null, this.hud);
+        this.title = this.game.add.text(DonutLayout.block * 4, 0, "Decimal Donuts", null, this.hud);
+    };
+    Game.prototype.centerRootGroup = function () {
+        var ppux = this.game.width / DonutLayout.width;
+        var ppuy = this.game.height / DonutLayout.height;
+        var ppu = Math.min(ppux, ppuy);
+        this.root = this.game.add.group();
+        this.root.x = ppux > ppuy ? this.game.width / 2 - ppu * DonutLayout.width / 2 : 0;
+        this.root.y = ppuy > ppux ? this.game.height / 2 - ppu * DonutLayout.height / 2 : 0;
+        this.root.scale.setTo(ppu, ppu);
+    };
+    Game.prototype.previewCaseSelected = function (alpha) {
+        for (var i = 0; i < this.cases.length; i++) {
+            if (i <= this.case_selected) {
+                this.cases[i].alpha = 1;
+            }
+            else {
+                this.cases[i].alpha = alpha;
+            }
+        }
     };
     Game.prototype.update = function () {
     };
@@ -89,13 +164,26 @@ var Game = /** @class */ (function () {
             this.game.rnd.pick(["sprinkles_1", "sprinkles_2", "sprinkles_3", "sprinkles_4", "sprinkles_5", "stripes_1", "stripes_2", "stripes_3"])
         ];
     };
+    Game.prototype.caseDonuts = function () {
+        var _this = this;
+        var count = this.donuts.length;
+        var cased = 0;
+        var hand_positions = [1, 0, 0, 1, 2, 2, 1, 0, 0, 1];
+        var moveTen = function () {
+            for (var i = 0; i < 10; i++) {
+                _this.donuts[cased + i].x = DonutLayout.block * i + DonutLayout.block / 2;
+                _this.donuts[cased + i].y = DonutLayout.block * hand_positions[i] + DonutLayout.block / 2;
+            }
+            _this.hands.y = _this.donuts_cases.y - _this.donuts_hands.y + _this.cases[0].y + Math.floor((cased) / 10) * DonutLayout.block;
+            cased += 10;
+            if (count - cased >= 10) {
+                moveTen();
+            }
+        };
+        moveTen();
+    };
     return Game;
 }());
-var Donut = /** @class */ (function () {
-    function Donut() {
-    }
-    return Donut;
-}());
 window.onload = function () {
-    var game = new Game();
+    var game = new Game(window.innerWidth, window.innerHeight);
 };
--- content/decimal-system/Game.ts
+++ content/decimal-system/Game.ts
@@ -1,28 +1,57 @@
-/// <reference path="../../typescript/phaser.d.ts" />
+/// <reference path="../../typescript/phaser.comments.d.ts" />
 
+class DonutLayout {
+    static width = 900
+    static height = 1500
+    static hud = 100
+
+    static donut = 66
+    static block = DonutLayout.donut + 10
+    static donuts_hands = DonutLayout.block * 5
+    static number_garbage = DonutLayout.block * 3
+    static donut_cases_padding = 50
+}
+
+enum State {
+    WAITING_FOR_CASE,
+    CASE_SELECTED,
+}
 
 class Game {
 
 
     game: Phaser.Game
+
+    root: Phaser.Group
+    hud: Phaser.Group
+    donuts_hands: Phaser.Group
+    number: Phaser.Group
+    donuts_cases: Phaser.Group
+
+    timer: Phaser.Text
+    title: Phaser.Text
+    stars: Phaser.Sprite[]
+
     donuts: Phaser.SpriteBatch[] = []
-    donut_holders: Phaser.Group[] = []
+    hands: Phaser.Group
     right_hand: Phaser.Sprite
     left_hand: Phaser.Sprite
-    hands: Phaser.Group
 
-    aspect_ratio = { width: 900, height: 1600 }
-    donuts_center = { x: 450, y: 180 }
-    donuts_radius = 130
-    donut_size = 62
-    donut_block = this.donut_size + 8
+    garbage: Phaser.Sprite
 
-    constructor() {
-        this.game = new Phaser.Game(this.aspect_ratio.width, this.aspect_ratio.height, Phaser.AUTO, "content", {
+    cases: Phaser.Group[] = []
+
+    case_selected: number
+    selection_made = false
+
+    constructor(width: number, height: number) {
+
+        this.game = new Phaser.Game(width, height, Phaser.AUTO, "content", {
             preload: () => this.preload(),
             create: () => this.create(),
             update: () => this.update()
         })
+
 
     }
 
@@ -32,97 +61,138 @@ class Game {
     }
 
     create() {
-
-        let holder_group = this.game.add.group()
-        holder_group.x = 100
-        holder_group.y = 700
-
-        
-        let style = {
-            font: "32px Arial",
-            fill: "black",
-            wordWrap: true,
-            wordWrapWidth: this.donut_block,
-            align: "center"
-        };
-
-        let donut_scale = this.donut_size / this.game.cache.getFrameByName("donuts", "donut_1").width
-
-        let rect = new Phaser.Rectangle(holder_group.x, this.donut_block, this.donut_block * 9, 2* this.donuts_radius)
-
-        let circle = new Phaser.Circle(this.donuts_center.x, this.donuts_center.y, 2 * this.donuts_radius)
-
-        let rnd_point = new Phaser.Point()
-        for (let index = 0; index < 15; index++) {
-
-            let x = holder_group.x + (index % 10) * this.donut_block + this.game.rnd.realInRange(-this.donut_block/2, this.donut_block/2)
-            let y = this.donut_block + this.game.rnd.realInRange(0, 2 * this.donuts_radius)
-
-
-           
-
-
-            rect.random(rnd_point)
-            this.donuts[index] = this.game.add.spriteBatch(null, "donut_" + (index + 1))
-            this.rnd_donut().forEach((name, layer_index) => {
-                let layer: Phaser.Sprite = this.donuts[index].create(x + this.donut_block/2, y + this.donut_block / 2, "donuts", name)
-                layer.anchor.setTo(0.5, 0.5)
-                layer.scale.setTo(donut_scale, donut_scale)
-            });
-
-            //let text = this.game.add.text(x + this.donut_block / 2, y + this.donut_block / 2, String(index + 1), style, this.donut_holders[index])
-            //text.anchor.setTo(0.5, 0.5)
-
-        }
-
-        for (let index = 0; index < 100; index++) {
-
-            let x = (index % 10) * this.donut_block
-            let y = Math.floor(index / 10) * this.donut_block
-
-
-            this.donut_holders[index] = this.game.add.group(holder_group)
-
-            // let graphics = this.game.add.graphics(0, 0, holder_group)
-            // graphics.lineStyle(1, 0x585858, 0.6);
-            // graphics.drawRect(x, y, this.donut_block, this.donut_block)
-
-
-            //let text = this.game.add.text(x + this.donut_block / 2, y + this.donut_block / 2, String(index + 1), style, this.donut_holders[index])
-            //text.anchor.setTo(0.5, 0.5)
-
-            let donut_holder = this.donut_holders[index].create(x + this.donut_block / 2, y + this.donut_block / 2, "donuts", "donut_black")
-            donut_holder.anchor.setTo(0.5, 0.5)
-            donut_holder.scale.setTo(donut_scale, donut_scale)
-            donut_holder.alpha = 1
-
-        }
-
-        let hand_frame = this.game.cache.getFrameByName("donuts", "right_hand")
-        let hand_scale = this.donut_block * 4 / hand_frame.width
-
-        this.hands = this.game.add.group()
-        this.hands.alpha = 0.8
-
-        this.right_hand = this.game.add.sprite(this.donut_block * 5, 0, "donuts", "right_hand", this.hands)
-        this.right_hand.anchor.setTo(0.5, 0)
-        this.right_hand.scale.setTo(hand_scale, hand_scale)
-
-        this.left_hand = this.game.add.sprite(0, 0, "donuts", "right_hand", this.hands)
-        this.left_hand.anchor.setTo(0.5, 0)
-        this.left_hand.scale.setTo(-hand_scale, hand_scale)
-
-        this.hands.x = holder_group.x +  this.donut_block * 2.5
-        this.hands.y = holder_group.y - this.right_hand.height
+        this.centerRootGroup();
+        this.initHud();
+        this.initDonutHands();
+        this.initNumber();
+        this.initDonutCases();
+        this.initGarbage();
 
     }
 
+
+    private initNumber() {
+        this.number = this.game.add.group(this.root, "number_garbage");
+        this.number.y = this.donuts_hands.y + DonutLayout.donuts_hands;
+    }
+
+    private initDonutCases() { 
+        let donut_scale = DonutLayout.donut / this.game.cache.getFrameByName("donuts", "donut_1").width
+    
+
+
+        this.donuts_cases = this.game.add.group(this.root, "donuts_cases");
+        this.donuts_cases.y = this.number.y + DonutLayout.number_garbage;
+        this.donuts_cases.x = DonutLayout.donut_cases_padding;
+        for (let index = 0; index < 100; index++) {
+            let x = (index % 10) * DonutLayout.block;
+            let y = Math.floor(index / 10) * DonutLayout.block;
+            this.cases[index] = this.game.add.group(this.donuts_cases);
+            this.cases[index].x = x;
+            this.cases[index].y = y;
+            let graphics = this.game.add.graphics(0, 0, this.cases[index]);
+            graphics.lineStyle(3, 0x404040, 1);
+            graphics.drawRect(0, 0, DonutLayout.block, DonutLayout.block);
+            //let text = this.game.add.text(x + this.donut_block / 2, y + this.donut_block / 2, String(index + 1), style, this.donut_holders[index])
+            //text.anchor.setTo(0.5, 0.5)
+            let donut_holder: Phaser.Sprite = this.cases[index].create(DonutLayout.block / 2, DonutLayout.block / 2, "donuts", "donut_black");
+            donut_holder.anchor.setTo(0.5, 0.5);
+            donut_holder.scale.setTo(donut_scale, donut_scale);
+            donut_holder.alpha = 1;
+            
+            donut_holder.data.index = index
+
+            donut_holder.inputEnabled = true
+            donut_holder.events.onInputOver.add(() => {
+                if (this.selection_made == false) {
+                    this.case_selected = donut_holder.data.index
+                    this.previewCaseSelected(0.3)
+                    console.log("Over ", donut_holder.data.index)
+                }
+                
+            })
+            donut_holder.events.onInputUp.add(() => {
+                this.case_selected = donut_holder.data.index
+                this.selection_made = true
+                console.log("Up on ", donut_holder.data.index)
+                this.previewCaseSelected(0)
+                this.caseDonuts()
+            })
+        }
+    }
+
+    private initGarbage() {
+        let garbage_scale = DonutLayout.number_garbage / (this.game.cache.getFrameByName("donuts", "recycle_bin").height + 10);
+        this.garbage = this.game.add.sprite(DonutLayout.block * 10, 0, "donuts", "recycle_bin", this.donuts_cases);
+        this.garbage.anchor.setTo(0.5, 0);
+        this.garbage.scale.setTo(garbage_scale, garbage_scale);
+        this.garbage.alpha = 0;
+    }
+
+    private initDonutHands() {
+        let donut_scale = DonutLayout.donut / this.game.cache.getFrameByName("donuts", "donut_1").width
+        this.donuts_hands = this.game.add.group(this.root, "donuts_hands");
+        this.donuts_hands.y = DonutLayout.hud;
+        this.donuts_hands.x = DonutLayout.donut_cases_padding;
+        for (let index = 0; index < 10; index++) {
+            let x = (index % 10 + this.game.rnd.realInRange(-0.5, 0.5)) * DonutLayout.block;
+            let y = this.game.rnd.realInRange(0, DonutLayout.donuts_hands - DonutLayout.block);
+            this.donuts[index] = this.game.add.spriteBatch(this.donuts_hands, "donut_" + (index + 1));
+            this.donuts[index].position.x = x + DonutLayout.block / 2;
+            this.donuts[index].position.y = y + DonutLayout.block / 2;
+            this.rnd_donut().forEach((name, layer_index) => {
+                let layer: Phaser.Sprite = this.donuts[index].create(0, 0, "donuts", name);
+                layer.anchor.setTo(0.5, 0.5);
+                layer.scale.setTo(donut_scale, donut_scale);
+            });
+            
+        }
+        this.initHands();
+    }
+
+    private initHands() {
+        this.hands = this.game.add.group(this.donuts_hands, "hands");
+        this.hands.alpha = 0.8;
+        let hand_scale = DonutLayout.donuts_hands / this.game.cache.getFrameByName("donuts", "right_hand").width;
+        this.right_hand = this.game.add.sprite(DonutLayout.donuts_hands, 0, "donuts", "right_hand", this.hands);
+        this.right_hand.scale.setTo(hand_scale, hand_scale);
+        this.left_hand = this.game.add.sprite(DonutLayout.donuts_hands/2, 0, "donuts", "right_hand", this.hands);
+        this.left_hand.anchor.setTo(0.5, 0);
+        this.left_hand.scale.setTo(-hand_scale, hand_scale);
+    }
+
+    private initHud() {
+        this.hud = this.game.add.group(this.root, "hud");
+        this.hud.x = DonutLayout.donut_cases_padding;
+        this.timer = this.game.add.text(0, 0, "1:30", null, this.hud);
+        this.title = this.game.add.text(DonutLayout.block * 4, 0, "Decimal Donuts", null, this.hud);
+    }
+
+    private centerRootGroup() {
+        let ppux = this.game.width / DonutLayout.width;
+        let ppuy = this.game.height / DonutLayout.height;
+        let ppu = Math.min(ppux, ppuy);
+        this.root = this.game.add.group();
+        this.root.x = ppux > ppuy ? this.game.width / 2 - ppu * DonutLayout.width / 2 : 0;
+        this.root.y = ppuy > ppux ? this.game.height / 2 - ppu * DonutLayout.height / 2 : 0;
+        this.root.scale.setTo(ppu, ppu);
+    }
+
+    private previewCaseSelected(alpha: number) {
+        for(let i = 0; i < this.cases.length; i++) {
+            if (i <= this.case_selected) {
+                this.cases[i].alpha = 1
+            } else {
+                this.cases[i].alpha = alpha
+            }
+        }
+    }
 
     update(): any {
 
     }
 
-    rnd_donut(): Array<string> {
+    private rnd_donut(): Array<string> {
         return [
             this.game.rnd.pick(["donut_1", "donut_1", "donut_2", "donut_3"]),
             this.game.rnd.pick(["glazing_1", "glazing_2", "glazing_3", "glazing_4", "glazing_5", "glazing_6",
@@ -131,16 +201,30 @@ class Game {
         ]
     }
 
-}
+    private caseDonuts() {
+        let count = this.donuts.length
+        let cased = 0
+        let hand_positions = [1, 0, 0, 1, 2, 2, 1, 0, 0, 1]
+        let moveTen = () => {
+            for(let i =0; i < 10; i++) {
+                this.donuts[cased + i].x = DonutLayout.block * i + DonutLayout.block/2
+                this.donuts[cased + i].y = DonutLayout.block * hand_positions[i] + DonutLayout.block/2
+            }
+            this.hands.y = this.donuts_cases.y - this.donuts_hands.y + this.cases[0].y + Math.floor((cased)/10) * DonutLayout.block      
+            cased += 10
+            if (count - cased >= 10) {
+                moveTen()
+            }
 
-class Donut {
-    base: Number
-    glazing: Number
-    glazing_zigzag: Number
-    sprinkles: Number
-    stripes: Number
+        }
+        
+        moveTen()
+    }
+
 }
 
 window.onload = () => {
-    var game = new Game()
+
+    var game = new Game(window.innerWidth, window.innerHeight)
 }
+
